2882cc4ed1a7c7a3ec2a1b37807172d264296cf3
BRUNO DUILIO MASSACCESI <48590334@ort>
2025-05-23 11:30:50 -0300

    asd

--- frontend/src/Views/IniciarSesion/IniciarSesion.jsx
+++ frontend/src/Views/IniciarSesion/IniciarSesion.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, TextInput, ImageBackground, TouchableOpacity, Pressable, Image, Button } from 'react-native';
 import { Link } from "react-router-dom";
 
 
 const IniciarSesion = () => {
+  const imgLocal = require('./assets/Fondo1.avif');
   return (
     <>
 
+
       
-        <Link to={"/Graficos"}> Graficos </Link>
+      
     </>
   );
 };
