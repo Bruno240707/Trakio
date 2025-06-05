406fa20843927755ad9e4a93ee6d2d0e032799fd
BrunoDuilioMassaccesi <massaccesibrunooo@gmail.com>
2025-06-05 10:46:28 -0300

    avances view infomracion + diseño

--- frontend/src/Views/Informacion/Informacion.jsx
+++ frontend/src/Views/Informacion/Informacion.jsx
@@ -5,11 +5,14 @@ const Informacion = () => {
         <>
             <img src="../src/Imagenes/fondoInfo.png" alt="FondoInformacion" className="fondoInfo" />
 
-            <div class="contenedorPrincipal">
+            <div class="contenidoInfo">
 
                 <h1 class="tituloInfo">Sobre Trako</h1>
-
-
+                <h2 class="sloganInfo">La seguridad y la gestión, en un solo sistema.</h2>
+                <h3 class="qInfo">¿Que somos?</h3>
+                <p class="textoInfoL">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente </p>
+                <hr class="linea"></hr>
+                <h3 class="qInfo">¿Quienes somos?</h3>
             </div>
 
 
