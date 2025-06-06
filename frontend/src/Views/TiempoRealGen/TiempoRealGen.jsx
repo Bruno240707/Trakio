bf0b4c2825a37b3e600a1281177b5309197f6734
BRUNO NAHUEL MARASI <48113202@ort>
2025-06-06 09:47:44 -0300

    avanzoTiempoReal

--- frontend/src/Views/TiempoRealGen/TiempoRealGen.jsx
+++ frontend/src/Views/TiempoRealGen/TiempoRealGen.jsx
@@ -1,48 +1,45 @@
-
 import { useEffect, useState } from "react";
-import "./TiempoRealGen.css"
+import "./TiempoRealGen.css";
 import CardTiempoReal from "../../Componentes/CardTiempoReal/index";
 
 const TiempoRealGen = () => {
+  const [dataEntradasSalidas, setDataEntradasSalidas] = useState([]);
 
-    const [dataEntradasSalidas, setDataEntradasSalidas] = useState([])
+  useEffect(() => {
+    const fetchData = () => {
+      fetch("http://localhost:3001/api/entradasSalidasTiempoReal")
+        .then((res) => res.json())
+        .then((data) => setDataEntradasSalidas(data))
+        .catch((err) => console.error("Error al cargar la API:", err));
+    };
 
-    useEffect(() => {
-        const fetchData = () => {
-          fetch("http://localhost:3001/api/entradasSalidasTiempoReal")
-            .then((res) => res.json())
-            .then((data) => setDataEntradasSalidas(data))
-            .catch((err) => console.error("Error al cargar la API:", err));
-        };
-    
-        fetchData();
-    
-        const intervalId = setInterval(fetchData, 5000);
-    
-        return () => clearInterval(intervalId);
-      }, []);
-      
-    return (
-        <>
-        <div>
-            <div>
-                <h1>Entradas y Salidas en Tiempo Real</h1>
-            </div>
+    fetchData();
+    const intervalId = setInterval(fetchData, 5000);
+    return () => clearInterval(intervalId);
+  }, []);
 
-            <div>
-                {dataEntradasSalidas.map(d => (
-                    <CardTiempoReal
-                    hora={d.hora}
-                    nombre={d.nombreEmpleado}
-                    tipo={d.tipo}
-                    />
-                ))}
-            </div>
-            
+  return (
+    <div className="tiempo-real-container">
+      <h1>Entradas y Salidas en Tiempo Real</h1>
 
-        </div>
-        </>
-    )
-}
+      <div className="lista-registros">
+        {dataEntradasSalidas.map((d, i) => (
+          <div key={i} className="registro-completo">
+            {d.tipo === "salida" ? (
+            <img src="../src/Imagenes/salida.png" className="icono-tipo" />
+            ) : (
+            <img src="../src/Imagenes/entrada.png" className="icono-tipo" />
+            )}
+            <CardTiempoReal
+              hora={d.hora}
+              nombre={d.nombreEmpleado}
+              tipo={d.tipo}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default TiempoRealGen
+export default TiempoRealGen;
