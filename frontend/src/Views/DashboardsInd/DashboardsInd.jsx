143920cef4454e3a46cf0ca3e1a0d03ed540228e
BrunoDuilioMassaccesi <massaccesibrunooo@gmail.com>
2025-06-11 09:51:03 -0300

    inserccion de graficos en vistas x usuario"

--- frontend/src/Views/DashboardsInd/DashboardsInd.jsx
+++ frontend/src/Views/DashboardsInd/DashboardsInd.jsx
@@ -1,149 +1,143 @@
 import { Link } from "react-router-dom"
+import { useState, useEffect } from "react"
+import DoughnutChart from "../../Componentes/DoughnutChart/index"
+import LineChart from "../../Componentes/LineChart/index"
+import BarChart from "../../Componentes/BarChart/index"
 import "./DashboardsInd.css"
 
 const DashboardsInd = () => {
+  const [lineData, setLineData] = useState([])
+  const [doughnutData, setDoughnutData] = useState([])
+  const [barData, setBarData] = useState([])
 
-    return (
-        <>
-            <div class="container">
-                <aside class="sidebar">
-                <input class="dropdown" type="text" placeholder="Empleados..."/>
+  useEffect(() => {
+    fetch("http://localhost:3001/api/doughnutData")
+      .then((res) => res.json())
+      .then((data) => setDoughnutData(data))
+      .catch((err) => console.error("Error al cargar la API:", err))
 
-                <ul class="user-list">
-                    <li><button class="delete">✕</button> Marcelo Godoy</li>
-                    <li><button class="delete">✕</button> Fran Marap</li>
-                    <li><button class="delete">✕</button> Lionel Andres</li>
-                    <li><button class="delete">✕</button> Eitan Mas</li>
-                    <li><button class="delete">✕</button> Nicolas Blaser</li>
-                    <li><button class="delete">✕</button> Marcelo Godoy</li>
-                    <li><button class="delete">✕</button> Fran Marap</li>
-                    <li><button class="delete">✕</button> Lionel Andres</li>
-                    <li><button class="delete">✕</button> Eitan Mas</li>
-                    <li><button class="delete">✕</button> Nicolas Blaser</li>
-                    <li><button class="delete">✕</button> Marcelo Godoy</li>
-                    <li><button class="delete">✕</button> Fran Marap</li>
-                    <li><button class="delete">✕</button> Lionel Andres</li>
-                    <li><button class="delete">✕</button> Eitan Mas</li>
-                    <li><button class="delete">✕</button> Nicolas Blaser</li>
-                    <li><button class="delete">✕</button> Marcelo Godoy</li>
-                    <li><button class="delete">✕</button> Fran Marap</li>
-                    <li><button class="delete">✕</button> Lionel Andres</li>
-                    <li><button class="delete">✕</button> Eitan Mas</li>
-                    <li><button class="delete">✕</button> Nicolas Blaser</li>
-                </ul>
+    fetch("http://localhost:3001/api/lineData")
+      .then((res) => res.json())
+      .then((data) => setLineData(data))
+      .catch((err) => console.error("Error al cargar la API:", err))
 
-                <div class="vista-general">
-                    <Link to={"/DashboardsGen"}>Vista General</Link>
-                </div>
-                </aside>
-
-                <main class="main-content">
-                <h1>Informacion de Empleado</h1>
-                <div class="employee-card">
-                    <div class="employee-header">
-                    <button class="employee-name">Francisco Marapode</button>
-                    <img src="https://randomuser.me/api/portraits/men/75.jpg" alt="Empleado" class="employee-photo" />
-                    </div>
-
-                    <div class="grid">
-                        <div class="card"></div>
-                        <div class="card"></div>
-                        <div class="card"></div>
-                        <div class="card"></div>
-                        <div class="card"></div>
-                        <div class="card"></div>
-                        <div class="card"></div>
-                        <div class="card"></div>
-                        <div class="card"></div>
-                        <div class="card"></div>
-                        <div class="card"></div>
-                        <div class="card"></div>
-                    </div>
-                </div>
-                </main>
-            </div>
-        </>
-    )
-}
-
-export default DashboardsInd
-
-
-/*import { Link } from "react-router-dom";
-import { useState } from "react";
-import "./DashboardsInd.css";
-
-const DashboardsInd = () => {
-  const [users, setUsers] = useState([
-    "Marcelo Godoy",
-    "Fran Marap",
-    "Lionel Andres",
-    "Eitan Mas",
-    "Nicolas Blaser",
-    "Marcelo Godoy",
-    "Fran Marap",
-    "Lionel Andres",
-    "Eitan Mas",
-    "Nicolas Blaser",
-    "Marcelo Godoy",
-    "Fran Marap",
-    "Lionel Andres",
-    "Eitan Mas",
-    "Nicolas Blaser",
-    "Marcelo Godoy",
-    "Fran Marap",
-    "Lionel Andres",
-    "Eitan Mas",
-    "Nicolas Blaser",
-  ]);
-
-  const handleDelete = (index) => {
-    const updatedUsers = users.filter((_, i) => i !== index);
-    setUsers(updatedUsers);
-  };
+    fetch("http://localhost:3001/api/barData")
+      .then((res) => res.json())
+      .then((data) => setBarData(data))
+      .catch((err) => console.error("Error al cargar la API:", err))
+  }, [])
 
   return (
     <>
-      <div className="container">
-        <aside className="sidebar">
-          <input className="dropdown" type="text" placeholder="Usuarios..." />
+      <div class="container">
+        <aside class="sidebar">
+          <input class="dropdown" type="text" placeholder="Empleados..." />
 
-          <ul className="user-list">
-            {users.map((user, index) => (
-              <li key={index}>
-                <button className="delete" onClick={() => handleDelete(index)}>✕</button> {user}
-              </li>
-            ))}
+          <ul class="user-list">
+            <li>
+              <button class="delete">✕</button> Marcelo Godoy
+            </li>
+            <li>
+              <button class="delete">✕</button> Fran Marap
+            </li>
+            <li>
+              <button class="delete">✕</button> Lionel Andres
+            </li>
+            <li>
+              <button class="delete">✕</button> Eitan Mas
+            </li>
+            <li>
+              <button class="delete">✕</button> Nicolas Blaser
+            </li>
+            <li>
+              <button class="delete">✕</button> Marcelo Godoy
+            </li>
+            <li>
+              <button class="delete">✕</button> Fran Marap
+            </li>
+            <li>
+              <button class="delete">✕</button> Lionel Andres
+            </li>
+            <li>
+              <button class="delete">✕</button> Eitan Mas
+            </li>
+            <li>
+              <button class="delete">✕</button> Nicolas Blaser
+            </li>
+            <li>
+              <button class="delete">✕</button> Marcelo Godoy
+            </li>
+            <li>
+              <button class="delete">✕</button> Fran Marap
+            </li>
+            <li>
+              <button class="delete">✕</button> Lionel Andres
+            </li>
+            <li>
+              <button class="delete">✕</button> Eitan Mas
+            </li>
+            <li>
+              <button class="delete">✕</button> Nicolas Blaser
+            </li>
+            <li>
+              <button class="delete">✕</button> Marcelo Godoy
+            </li>
+            <li>
+              <button class="delete">✕</button> Fran Marap
+            </li>
+            <li>
+              <button class="delete">✕</button> Lionel Andres
+            </li>
+            <li>
+              <button class="delete">✕</button> Eitan Mas
+            </li>
+            <li>
+              <button class="delete">✕</button> Nicolas Blaser
+            </li>
           </ul>
 
-          <div className="vista-general">
+          <div class="vista-general">
             <Link to={"/DashboardsGen"}>Vista General</Link>
           </div>
         </aside>
 
-        <main className="main-content">
-          <h1>Información de Empleado</h1>
-          <div className="employee-card">
-            <div className="employee-header">
-              <button className="employee-name">Francisco Marapode</button>
+        <main class="main-content">
+          <h1>Informacion de Empleado</h1>
+          <div class="employee-card">
+            <div class="employee-header">
+              <button class="employee-name">Francisco Marapode</button>
               <img
                 src="https://randomuser.me/api/portraits/men/75.jpg"
                 alt="Empleado"
-                className="employee-photo"
+                class="employee-photo"
               />
             </div>
 
-            <div className="grid">
-              {Array.from({ length: 12 }).map((_, i) => (
-                <div className="card" key={i}></div>
-              ))}
+            <div class="grid">
+              <div class="card">
+                <LineChart lineData={lineData} />
+              </div>
+              <div class="card">
+                <DoughnutChart doughnutData={doughnutData} />
+              </div>
+              <div class="card">
+                <BarChart barData={barData} />
+              </div>
+              <div class="card"></div>
+              <div class="card"></div>
+              <div class="card"></div>
+              <div class="card"></div>
+              <div class="card"></div>
+              <div class="card"></div>
+              <div class="card"></div>
+              <div class="card"></div>
+              <div class="card"></div>
             </div>
           </div>
         </main>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default DashboardsInd;
-*/
+export default DashboardsInd
