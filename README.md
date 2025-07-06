ee9bf201acc78b5604c1662aff02f0724df84ca6
Bruno2407 <brunonahuelmarasi1@gmail.com>
2025-07-06 20:20:45 -0300

    contacto funcional

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # Trakio
 
-conexion a base de datos
-se desabhilito error404
-estilar View/DashboardsGen
-no aparecen fotos sacadas de src/imagenes     ej: logo en el iniciar sesion de trakio
+conexion a base de datos // HECHO
+se desabhilito error404 // HECHO
+no aparecen fotos sacadas de src/imagenes // HECHO
+estilar bien contacto y que sea funcional
+
+
+estilar View/DashboardsGen 
 gif del dashboard home
 meter mas graficos
 hacer funcional los empleados y lo q vimos en EFSI de crear views con id para cada empleado
 investigar recuperar contraseña, q mande el mail
 agregar mucha informacion y fotos formales en view informacion
-estilar bien contacto y que sea funcional
 
 
 
--- frontend/src/Views/Contacto/Contacto.jsx
+++ frontend/src/Views/Contacto/Contacto.jsx
@@ -1,34 +1,46 @@
 import { useState } from 'react';
+import emailjs from "@emailjs/browser";
 import './Contacto.css';
 
-export default function Contacto() {
-  const [formData, setFormData] = useState({
-    nombre: '',
-    email: '',
-    mensaje: '',
-  });
+const Contacto = () => {
 
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
+  const [email, setEmail] = useState("")
+  const [mensaje, setMensaje] = useState("")
+  const [nombre, setNombre] = useState("")
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    alert('Consulta enviada correctamente, te enviaremos la respuesta via mail !');
-    setFormData({
-      nombre: '',
-      email: '',
-      mensaje: '',
-    });
-  };
+  e.preventDefault();
+
+      emailjs
+        .send(
+          'service_4a7w54d',
+          'template_pn8s8vk',
+          {
+            user_nombre: nombre,
+            user_email: email,
+            message: mensaje,
+          },
+          'pQTqNKaaHQW1qydJY'
+        )
+        .then(
+          (result) => {
+            console.log('Correo enviado', result.text);
+            alert('Mensaje enviado con éxito');
+            setEmail('');
+            setMensaje('');
+            setNombre('');
+          },
+          (error) => {
+            console.error('Error al enviar', error);
+            alert('Hubo un problema al enviar el mensaje');
+          }
+        );
+    };
 
   return (
     <div className="contacto-container">
       <h1 className="contacto-titulo">¡ Contactanos !</h1>
-      <img src="../src/Imagenes/logo.png" alt="logo" class="logoContacto" />
+      <img src="../src/Imagenes/logo.png" alt="logo" className="logoContacto" />
       <form onSubmit={handleSubmit} className="contacto-formulario">
         <div className="form-grupo">
           <label htmlFor="nombre" className="form-label">Nombre:</label>
@@ -36,8 +48,8 @@ export default function Contacto() {
             type="text"
             id="nombre"
             name="nombre"
-            value={formData.nombre}
-            onChange={handleChange}
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
             required
             className="form-input"
           />
@@ -48,8 +60,8 @@ export default function Contacto() {
             type="email"
             id="email"
             name="email"
-            value={formData.email}
-            onChange={handleChange}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             required
             className="form-input"
           />
@@ -59,8 +71,8 @@ export default function Contacto() {
           <textarea
             id="mensaje"
             name="mensaje"
-            value={formData.mensaje}
-            onChange={handleChange}
+            value={mensaje}
+            onChange={(e) => setMensaje(e.target.value)}
             required
             className="form-textarea"
           />
@@ -70,3 +82,5 @@ export default function Contacto() {
     </div>
   );
 }
+
+export default Contacto
